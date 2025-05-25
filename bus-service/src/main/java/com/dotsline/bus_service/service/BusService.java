98f2846077f72f32016e7264bb0242e4018c6b95
Thithira-Paranawithana <153026117+Thithira-Paranawithana@users.noreply.github.com>
2025-05-26 01:43:24 +0530

    Initial commit

---
+++ bus-service/src/main/java/com/dotsline/bus_service/service/BusService.java
@@ -0,0 +1,41 @@
+package com.dotsline.bus_service.service;
+
+import com.dotsline.bus_service.dao.BusRepository;
+import com.dotsline.bus_service.model.Bus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BusService {
+
+    @Autowired
+    private BusRepository busRepository;
+
+    public Bus createBus(Bus bus) {
+        return busRepository.save(bus);
+    }
+
+    public List<Bus> getAllBuses() {
+        return busRepository.findAll();
+    }
+
+    public Optional<Bus> getBusById(Integer id) {
+        return busRepository.findById(id);
+    }
+
+    public Bus updateBus(Integer id, Bus updatedBus) {
+        Bus existingBus = busRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Bus not found with id: " + id));
+
+        existingBus.setBusNumber(updatedBus.getBusNumber());
+        existingBus.setType(updatedBus.getType());
+        existingBus.setTotalSeats(updatedBus.getTotalSeats());
+        existingBus.setOperatorName(updatedBus.getOperatorName());
+
+        return busRepository.save(existingBus);
+    }
+
+}
